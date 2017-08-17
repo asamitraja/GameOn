3bf4759d6660fe2b70597de3db1cbe593cc898c3
asamitraja <asamitraja@gmail.com>
2017-08-17 12:47:01 +0530

    change 1

--- src/LoginUtils.java
+++ src/LoginUtils.java
@@ -1,4 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class LoginUtils {
-
+	
+	
+	private Connection getCon(){
+		Connection con=null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameOn","root","");
+			
+		}catch(Exception e){}
+		return con;
+	}
+	
 }
